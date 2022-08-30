dc98df76ec0e77c58c555809a12dfd0af979fbeb
unchain123 <unchain123@kakao.com>
2022-08-30 17:28:21 +0900

    feat: 받아온 이미지 IconId를 네트워크요청으로 이미지로 변환하여서 일기리스트에 삽입하는 과정 추가

---
+++ Diary+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Diary+CoreDataClass.swift
+//  
+//
+//  Created by unchain, 웡빙 on 2022/08/30.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Diary)
+public class Diary: NSManagedObject {
+
+}
--- Diary/NetworkManager.swift
+++ Diary/NetworkManager.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 final class NetworkManager {
     static let shared = NetworkManager()
